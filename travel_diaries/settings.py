ec934d0f1c8c13afbad58f65a0cf4b20757cd92f
laisbsc <santanal@tcd.ie>
2025-07-28 21:48:43 +0100

    update database configuration

--- travel_diaries/settings.py
+++ travel_diaries/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'travel_diaries.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(conn_max_age=600u),
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
