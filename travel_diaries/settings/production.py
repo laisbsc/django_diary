3749deda95518e11a38ec35f0092737c2af53e2d
laisbsc <santanal@tcd.ie>
2026-04-15 16:25:10 +0100

    remove CSRF_TRUSTED_ORIGINS from env file

--- travel_diaries/settings/production.py
+++ travel_diaries/settings/production.py
@@ -4,9 +4,9 @@ from .base import *
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [h for h in os.environ.get('ALLOWED_HOSTS', '').split(',') if h]
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [h for h in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if h]
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
